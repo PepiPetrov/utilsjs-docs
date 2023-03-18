426410bf042f8c935dd83e4543e96deeec1893c8
PepiPetrov <pepi_petrov@outlook.com>
2023-03-18 16:30:28 +0200

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>UtilsJS</span>,
+  project: {
+    link: 'https://github.com/PepiPetrov/UtilsJS',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'UtilsJS - The JavaScript utility library',
+  },
+}
+
+export default config
